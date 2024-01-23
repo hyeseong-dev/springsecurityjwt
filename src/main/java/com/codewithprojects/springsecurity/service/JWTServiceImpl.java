4c92278128ba585142374459ca5942c5833b5a89
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-23 11:09:47 +0900

    ADD. comment for userDetails and JWTService

--- src/main/java/com/codewithprojects/springsecurity/service/JWTServiceImpl.java
+++ src/main/java/com/codewithprojects/springsecurity/service/JWTServiceImpl.java
@@ -14,6 +14,13 @@ import java.util.function.Function;
 
 @Service
 public class JWTServiceImpl implements JWTService{
+
+    /**
+     * 주어진 사용자의 상세 정보를 바탕으로 JWT 토큰을 생성합니다.
+     *
+     * @param userDetails 사용자의 상세 정보
+     * @return 생성된 JWT 토큰 문자열
+     */
     private String generateToken(UserDetails userDetails){
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
@@ -23,20 +30,45 @@ public class JWTServiceImpl implements JWTService{
                 .compact();
     }
 
+    /**
+     * JWT 토큰에서 사용자 이름(Username)을 추출합니다.
+     *
+     * @param token 분석할 JWT 토큰
+     * @return 토큰에서 추출된 사용자 이름
+     */
     public String ExtractUsername(String token){
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * JWT 서명에 사용될 키를 생성합니다.
+     *
+     * @return 생성된 서명 키
+     */
     private Key getSigningKey(){
         byte[] key = Decoders.BASE64.decode("superstrongsecret");
         return Keys.hmacShaKeyFor(key);
     }
 
+    /**
+     * 주어진 JWT 토큰에서 특정 클레임을 추출합니다.
+     *
+     * @param token 분석할 JWT 토큰
+     * @param claimsResolver 클레임을 추출하기 위한 함수
+     * @param <T> 추출될 클레임의 타입
+     * @return 추출된 클레임 값
+     */
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers){
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
     }
 
+    /**
+     * JWT 토큰에서 모든 클레임을 추출합니다.
+     *
+     * @param token 분석할 JWT 토큰
+     * @return 토큰에서 추출된 모든 클레임
+     */
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
     }
