fae561f1a455044fae15f532d07e0169b43a892e
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-25 12:46:50 +0900

    final stuff

--- src/main/java/com/codewithprojects/springsecurity/controller/AdminController.java
+++ src/main/java/com/codewithprojects/springsecurity/controller/AdminController.java
@@ -1,6 +1,10 @@
 package com.codewithprojects.springsecurity.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("hi user");
+    }
 }
